b2469d3fc4e16315b32ff53a939b33e0cfbd4571
Vinko Mlačić <vinkomlacic@outlook.com>
2019-08-22 15:25:29 +0200

    feat: initialize Spring Boot project

---
+++ src/main/java/org/geotag/geotagrestapi/GeotagRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.geotag.geotagrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotagRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeotagRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/geotag/geotagrestapi/GeotagRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.geotag.geotagrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeotagRestApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

9b66df15467aabd4c7a34d709a8fde6c9f867be7
Vinko Mlačić <vinkomlacic@outlook.com>
2019-08-31 21:14:31 +0200

    feat: changes to Image model

--- src/main/java/org/geotag/geotagrestapi/model/Image.java
+++ src/main/java/org/geotag/geotagrestapi/model/Image.java
@@ -1,10 +1,14 @@
 package org.geotag.geotagrestapi.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vividsolutions.jts.geom.Point;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
-import org.springframework.data.geo.Point;
+import org.geotag.geotagrestapi.utils.json.JsonToPointDeserializer;
+import org.geotag.geotagrestapi.utils.json.PointToJsonSerializer;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
@@ -15,6 +19,9 @@ import javax.validation.constraints.*;
 @FieldNameConstants
 public class Image {
     private static final String B64_REGEXP = "[a-zA-Z0-9+/=]*";
+
+    public Image() {
+    }
 
     public Image(final String title, final String description, final String filename, final String encodedFilename,
                  final Point location, final String deviceId, final String base64Content) {
@@ -49,16 +56,16 @@ public class Image {
 
     @Column
     @Size(max = 255, message = "Title is to big")
-    private final String title;
+    private String title;
 
     @Column (length = 1024)
     @Size(max = 1024, message = "Description is too big")
-    private final String description;
+    private String description;
 
     @Column(nullable = false)
     @NotNull(message = "Filename cannot be null")
     @Size(max = 255)
-    private final String filename;
+    private String filename;
 
     @Column(name = "encoded_filename", nullable = false)
     @Size(max = 255)
@@ -66,12 +73,14 @@ public class Image {
 
     @Column(nullable = false)
     @NotNull(message = "Location cannot be null")
-    private final Point location;
+    @JsonSerialize(using = PointToJsonSerializer.class)
+    @JsonDeserialize(using = JsonToPointDeserializer.class)
+    private Point location;
 
     @Column(name = "device_id", nullable = false)
     @NotNull(message = "Device ID cannot be null")
     @Size(max = 255)
-    private final String deviceId;
+    private String deviceId;
 
     @Transient
     @NotEmpty(message = "Image must have content")

b4f4a663d53dffcf61c63222de1a49144b0ae91a
Vinko Mlačić <vinkomlacic@outlook.com>
2019-09-07 00:41:38 +0200

    del: delete unused exceptions

--- src/main/java/org/geotag/geotagrestapi/exceptions/NotADirectoryException.java
+++
@@ -1,9 +0,0 @@
-package org.geotag.geotagrestapi.exceptions;
-
-import java.nio.file.Path;
-
-public class NotADirectoryException extends Exception {
-    public NotADirectoryException(final Path path) {
-        super(String.format("Path %s is not a directory", path.toString()));
-    }
-}
